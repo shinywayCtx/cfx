3780443c1ad51f2cfa671b6bbc4338f96a4b6e7d
shinywayctx <15858193405@126.com>
2016-01-01 23:08:07 +0800

    111111111

--- eclipse.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo eclipse开始...
-
-call mvn eclipse:clean eclipse:eclipse
-
-echo eclipse完成
-
-pause
--- install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo install开始...
-
-call mvn clean install -Dmaven.test.skip=true 
-
-echo install完成
-
-pause
